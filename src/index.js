f5d1778958b5cca1ed4bfe620bc74d1914674788
Iaroslav Gritsenko <yaroslav.gritsenko@instatsport.com>
2023-02-25 21:49:08 +0300

    done

--- src/index.js
+++ src/index.js
@@ -38,8 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let array = [];
+    let morse = '';
+
+    for (i = 0; i < expr.length; i = i + 10) {
+
+        if (expr.slice(i, i+2) === '**') {
+            array.push(' ');
+        } else for (j = 0; j < 9; j = j + 2) {
+            
+            if (expr.slice(i, i+10).slice(j, j+2) === '11') {
+                morse += '-';
+            } else if (expr.slice(i, i+10).slice(j, j+2) === '10') {
+                morse += '.';
+            }
+
+        } array.push(MORSE_TABLE[morse]);
+        morse = '';
+    }
+return array.join('');
 }
+
 
 module.exports = {
     decode
